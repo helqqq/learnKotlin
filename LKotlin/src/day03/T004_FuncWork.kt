ef157ef8444c246f1b8cadd66e34b5aa61301881
helena <helena.lq.he@pensees.ai>
2020-11-25 10:58:32 +0800

    kotlin 函数简单作业

---
+++ LKotlin/src/day03/T004_FuncWork.kt
@@ -0,0 +1,18 @@
+package day03
+
+import java.util.*
+
+//function 作业
+fun main(args: Array<String>) {
+    println(sayHello("张三"))
+    println(checkAge(20))
+}
+fun sayHello(name: String):String{
+    return "hello," + name
+}
+fun checkAge(age: Int): Boolean{
+    if(age > 18) return true else return false
+}
+fun saveLog(logLevel: Int){
+
+}
